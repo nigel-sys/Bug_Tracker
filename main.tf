25ad9728693ae837d602108d64ef1b4de2ae2466
Nigel <pratyushnbaxla79@gmail.com>
2022-11-26 14:23:03 +0000

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY
+}
