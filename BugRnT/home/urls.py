bd6426b34bebb1b97a13e53b6c99370502dbf372
Nigel <pratyushnbaxla79@gmail.com>
2022-11-28 20:19:34 +0000

    Report bug

--- BugRnT/home/urls.py
+++ BugRnT/home/urls.py
@@ -2,6 +2,9 @@ from django import views
 from django.urls import path
 from . import views
 
+app_name = "home"
+
 urlpatterns = [
-    path('', views.BugsListView.as_view(), name='list')
+    path('', views.BugsListView.as_view(), name='list'),
+    path("add-bug/", views.BugCreateView.as_view(), name="add"),
 ]
--- BugRnT/home/views.py
+++ BugRnT/home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, TemplateView
 from .models import Bug
+from django.urls import reverse_lazy
 
 # Create your views here.
-
 
 class BugsListView(ListView):
     model = Bug
     context_object_name = "Bugs_list"
+
+class BugCreateView(CreateView):
+    model = Bug
+    fields = "__all__"
+    success_url = reverse_lazy("home:list")
+
