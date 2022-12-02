d409fda9a3838de3b955913eedbeae2b281cd846
Nigel <pratyushnbaxla79@gmail.com>
2022-12-02 17:33:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,10 +25,53 @@ resource "aws_key_pair" "generated_key" {
 
 resource "aws_instance" "AWS-instance" {
   count = 1 
-  ami = "ami-096800910c1b781ba"
+  ami = "ami-001c1ab2631f48e96"
   instance_type = "t2.micro"
   key_name = aws_key_pair.generated_key.key_name
   tags = {
     Name = "Team15"
+  }
+  provisioner "remote-exec" {
+    inline = [
+"source newenv/bin/activate",
+"sudo git clone https://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@github.com/nigel-sys/Bug_Tracker.git",
+"cd Bug_Tracker",
+"cd BugRnT/",
+"python3 manage.py runserver 8000"
+    ]
+
+    connection {
+      type        = "ssh"
+      private_key = "${tls_private_key.AWS-instance.private_key_pem}"
+      user        = "ubuntu"
+      timeout     = "1m"
+      host = self.public_ip
+    }
+  }
+}
+
+resource "aws_security_group" "AWS-instance" {
+  name        = "grant ssh"
+  description = "grant ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
